9d57a61190bd3ef88b75ba30c454dc0caaa701f7
usmonxon <usmonxonsirochxonov@gmail.com>
2022-05-04 18:31:44 +0500

    five created

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Home.css";
 
 export default function Home(props) {
   return (
     <div className="home">
       <div className="container">
-        <div className="name">
-          <h1>HI, I'M USMONXON</h1>
-        </div>
         <div className="boxes">
-          <div className="box box1">
-            <a target={"_blank"} href="https://www.instagram.com/sirochxonov_usmonxon_official">
-              <img src="/images/instagram.png" alt="rasm1" />
-            </a>
+          <div className="box">
+            <h3 style={{ fontSize: "30px" }}>Hello, I'm</h3>
+            <h1>USMONXON SIROCHXONOV</h1>
+            <h2>Frontend Developer</h2>
+            {/* <h2>Student</h2> */}
+            <Link className="link1" to="/resume">
+              SEE MY WORK
+            </Link>
+            <div className="message">
+              <a
+                target={"_blank"}
+                href="https://www.instagram.com/sirochxonov_usmonxon_official"
+              >
+                <img src="/images/instagram.png" alt="rasm1" />
+              </a>
+              <a target={"_blank"} href="https://t.me/Maykl_skofild_2001">
+                <img src="/images/telegram.png" alt="rasm2" />
+              </a>
+              <a target={"_blank"} href="https://usmonxonsirchxonov@gmail.com">
+                <img src="/images/search.png" alt="rasm3" />
+              </a>
+              <a target={"_blank"} href="https://github.com/usmonxon-coder">
+                <img src="/images/github.png" alt="rasm4" />
+              </a>
+            </div>
           </div>
-          <div className="box box2">
-            <a target={"_blank"} href="https://t.me/Maykl_skofild_2001">
-              <img src="/images/telegram.png" alt="rasm2" />
-            </a>
-          </div>
-          <div className="box box3">
-            <a target={"_blank"} href="https://usmonxonsirchxonov@gmail.com">
-              <img src="/images/search.png" alt="rasm3" />
-            </a>
-          </div>
-          <div className="box box4">
-            <a target={"_blank"} href="https://github.com/usmonxon-coder">
-              <img src="/images/github.png" alt="rasm4" />
-            </a>
-          </div>
+          
         </div>
       </div>
     </div>
